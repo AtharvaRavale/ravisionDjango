e7e312ecb03cb8d1420e4de2b04e85e9f299fd33
you <you@example.com>
2024-12-10 13:22:34 +0530

    login/reg view

---
+++ Mainproject/account/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import *
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model=Contributor
+        fields=['username','password']
+
+
+class RegisterForm(forms.ModelForm):
+    confirm_password=forms.CharField(max_length=100,widget=forms.PasswordInput(
+        attrs={
+            'placeholder':'Enter Password',
+        }
+    ))
+
+    class Meta:
+        model=Contributor
+        exclude=['is_superuser','is_staff']
--- Mainproject/account/views.py
+++ Mainproject/account/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.views import View
+from .forms import RegisterForm,LoginForm
+from django.contrib.auth import authenticate,login
 # Create your views here.
+
+class Register(View):
+    def get(self,request):
+        form= RegisterForm()
+        return render(request,"accounts/register.html",{'form':form})
+
+    def post(self,request):
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            return render(request,"accounts/register.html",{'form':form})
+        
+
+
+
+
+class Login(View):
+    def get(self,request):
+        form= LoginForm()
+        return render(request,"accounts/login.html",{'form':form})
+
+    def post(self,request):
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            user=authenticate(form.cleaned_data['username'],form.cleaned_data['password'])
+            if user:
+                login(request,user)
+                return redirect('register')
+        return render(request,"accounts/login.html",{'form':form})
+
+
+
