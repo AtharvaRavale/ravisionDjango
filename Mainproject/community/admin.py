1682fb517ee0601a738280c8aade5f04bc594591
you <you@example.com>
2024-12-12 13:22:24 +0530

    models of community added

--- Mainproject/community/admin.py
+++ Mainproject/community/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Motive
 
 # Register your models here.
+
+
+@admin.register(Motive)
+class MotiveAdmin(admin.ModelAdmin):
+    list_display=('name','motive_type','created_at',)
+    list_filter=('motive_type',)
+    search_fields=('name',)
